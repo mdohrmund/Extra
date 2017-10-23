be3301794bec4bef75109ae797094546cefc0dd1
mdohrmund <31314575+mdohrmund@users.noreply.github.com>
2017-10-22 21:58:26 -0500

    Add files via upload

---
+++ src/array/Quiz.java
@@ -0,0 +1,174 @@
+package array;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Quiz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String user;
+		int choice;
+		Scanner input = new Scanner(System.in);
+	
+		user = JOptionPane.showInputDialog(null,"What does 'abuela' mean in English? \n1 mom \n2 brother \n3 grandma");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "The correct answer was 3");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 3");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'cantar' mean in English? \n1 to sing \n2 to drink \n3 to run");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+		
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'el centro comercial' mean in English? \n1 mall \n2 pencil \n3 tv show");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+		
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'comer' mean in English? \n1 to draw \n2 to write \n3 to eat");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "The correct answer was 3");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 3");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'cantar' mean in English? \n1 to sing \n2 to drink \n3 to run");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'hacer la tarea' mean in English? \n1 to exercise \n2 to do homework \n3 to shop" +
+				"");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'juegos de mesa' mean in English? \n1 board games \n2 to play football \n3 table");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 1");
+		}
+			
+		
+		user = JOptionPane.showInputDialog(null,"What does 'leer' mean in English? \n1 to fish \n2 to read \n3 to hunt");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'playa' mean in English? \n1 boy \n2 pool \n3 beach");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "The correct answer was 3");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 3");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'trabajar' mean in English? \n1 to be short \n2 to work \n3 to sleep");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+		
+		
+		user = JOptionPane.showInputDialog(null,"What does 'siempre' mean in English? \n1 always \n2 sometimes \n3 never");
+		choice = Integer.parseInt(user);
+		if(choice == 1){
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+		if(choice == 2) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+		if(choice == 3) {
+			JOptionPane.showMessageDialog(null, "The correct answer was 2");
+		}
+		
+		JOptionPane.showMessageDialog(null, "Your score is " + score + ". Would you like to play again?");
+		if("Yes".equalsIgnoreCase()) {
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/array/Quiz2.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Quiz2 {
+	private static String[] questions = {"What does abuela mean in English? \nA) mom \nB) brother \nC) Grandma", "What does cantar mean in English? \nA to sing \nB to drink \nC to run", 
+		"What does el centro comercial mean in English? \nA mall \nB pencil \nC tv show", "What does comer mean in English? \nA to draw \nB to write \nC to eat",
+		"What does cantar mean in English? \nA to sing \nB to drink \nC to run", "What does hacer la tarea mean in English? \nA to exercise \nB to do homework \nC to shop",
+		"What does juegos de mesa mean in English? \n1 board games \n2 to play football \n3 table"};
+	private static String[] answers = {"C", "A", "A", "C", "A", "B", "A"};
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
